679c11d22bf2c43c636760ae605c4af4d3ccfca5
Klotske <ilya@mokhov.vg>
2020-11-03 13:46:13 +0300

    Fix pep8 & add categories

--- main.py
+++ main.py
@@ -1,74 +1,74 @@
 import csv
 
-def getTextValue(message):
+def get_text_value(message):
     while True:
         value = input(message)
         if value == '' or value.isalpha():
             break
     return value
 
-def getNumValue(message):
+def get_num_value(message):
     while True:
         value = input(message)
         if value == '' or value.isdigit():
             break
     return value
 
-def checkYear(userYear, gameYear):
-    if userYear == '': return True
-    return True if gameYear <= int(userYear) else False
+def check_year(user_year, game_year):
+    if user_year == '': return True
+    return True if game_year <= int(user_year) else False
 
-def checkAge(userAge, gameAge):
-    if userAge == '': return True
-    return True if gameAge <= int(userAge) else False
+def check_age(user_age, game_age):
+    if user_age == '': return True
+    return True if game_age <= int(user_age) else False
 
-def checkGenre(userGenres, gameGenres):
-    if userGenres == ['']: return True
-    return True if any(uGenre in gameGenres for uGenre in userGenres) else False
+def check_genre(user_genres, game_genres):
+    if user_genres == ['']: return True
+    return True if any(uGenre in game_genres for uGenre in user_genres) else False
 
-def checkCategory(userCategory, gameCategory):
-    if userCategory == ['']: return True
-    return True if any(uCategory in gameCategory for uCategory in userCategory) else False
+def check_category(user_category, game_category):
+    if user_category == ['']: return True
+    return True if any(uCategory in game_category for uCategory in user_category) else False
 
-def checkPlatform(userPlatform, gamePlatform):
-    if userPlatform == ['']: return True
-    return True if any(uPlatform in gamePlatform for uPlatform in userPlatform) else False
+def check_platform(user_platform, game_platform):
+    if user_platform == ['']: return True
+    return True if any(uPlatform in game_platform for uPlatform in user_platform) else False
 
-def checkPrice(userPrice, gamePrice):
-    if userPrice == '': return True
-    return True if gamePrice <= float(userPrice) else False
+def check_price(user_price, game_price):
+    if user_price == '': return True
+    return True if game_price <= float(user_price) else False
 
-def checkRating(userRating, gameRating):
-    if userRating == '': return True
-    return True if gameRating >= float(userRating) else False
+def check_rating(user_rating, game_rating):
+    if user_rating == '': return True
+    return True if game_rating >= float(user_rating) else False
 
 print("Вас приветсвтвует мастер поиска игр!")
 print("Для того, чтобы мы смогли посоветовать вам игры, Вам необхожимо пройти краткий опрос.")
 print("Для выбора нескольких вариантов, введите их через запятую. Чтобы пропустить вопрос нажмите Enter")
 
-years = getNumValue("Какой максимальный год выпуска игры вас интересует? ")
-genres = getTextValue("Какой жанр игр вас интересует (Action, RPG)? ").split(',')
-age = getNumValue("Укажите минимальное ограничение по возрасту: ")
-categories = getTextValue("Укажите категорию игры (): ").split(',')
-platforms = getTextValue("На какую платформу планируется установка (windows, mac, linux)? ").split(',')
-prices = getNumValue("Укажите максимальную цену игры ($): ")
-ratings = getNumValue("Укажите минимальный процент положительных отзывов: ")
+years = get_num_value("Какой максимальный год выпуска игры вас интересует? ")
+genres = get_text_value("Какой жанр игр вас интересует (Action, RPG)? ").split(',')
+age = get_num_value("Укажите минимальное ограничение по возрасту: ")
+categories = get_text_value("Укажите категорию игры (Multiplayer, Online): ").split(',')
+platforms = get_text_value("На какую платформу планируется установка (windows, mac, linux)? ").split(',')
+prices = get_num_value("Укажите максимальную цену игры ($): ")
+ratings = get_num_value("Укажите минимальный процент положительных отзывов: ")
 
 
 with open("steam.csv", encoding="utf-8") as f, open("out.txt", "w", encoding="utf-8") as out:
     reader = csv.reader(f)
     collumns = next(reader)
     for row in reader:
-        gName = row[1]
-        gYear = int(row[2].split('-')[0])
-        gPlatform = row[6].split(';')
-        gAge = int(row[7])
-        gCategory = row[8].split(';')
-        gGenres = row[9].split(';')
-        gPrice = float(row[17])
-        gRating = float(int(row[12])/(int(row[12])+int(row[13])))
+        game_name = row[1]
+        game_year = int(row[2].split('-')[0])
+        game_platform = row[6].split(';')
+        game_age = int(row[7])
+        game_category = row[8].split(';')
+        game_genres = row[9].split(';')
+        game_price = float(row[17])
+        game_rating = float(int(row[12])/(int(row[12])+int(row[13])))
 
-        if (checkYear(years, gYear) and checkGenre(genres, gGenres) and checkAge(age, gAge)):
-            if (checkCategory(categories, gCategory) and checkPlatform(platforms, gPlatform) and checkPrice(prices, gPrice)):
-                if (checkRating(ratings, gRating)):
-                    out.write(gName + "\n")
+        if (check_year(years, game_year) and check_genre(genres, game_genres) and check_age(age, game_age)):
+            if (check_category(categories, game_category) and check_platform(platforms, game_platform) and check_price(prices, game_price)):
+                if (check_rating(ratings, game_rating)):
+                    out.write(game_name + "\n")
